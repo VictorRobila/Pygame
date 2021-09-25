0fa07c9f2ac1f4fabe0e0d561deb011d2618e330
VictorRobila <victorrobila@hunterschools.org>
2021-09-25 15:06:45 -0500

    Update pyweek.py

--- pyweek.py
+++ pyweek.py
@@ -10,8 +10,8 @@ clock = pygame.time.Clock()
 start_time = 0
 screen_type = 'main_menu'
 back_counter = 111
-fps = 60
-fps_presses = 0
+diff_presses = 2
+difficulty = 'Normal'
 volume = 5
 vol_presses = 0
 player_gravity = 0
@@ -90,11 +90,15 @@ def gen_random(structure):
 
 
 def display_hp(enemies_killed):
+    global current_room_number
     score_surface = crimson_medium.render(f'Health: {hp}',True,white)
-    score_rectangle = score_surface.get_rect(topleft=(0,0))
+    score_rectangle = score_surface.get_rect(topleft=(50,0))
     screen.blit(score_surface,score_rectangle)
-    score1_surface = crimson_medium.render('Enemies Killed:'+str(enemies_killed),True,white)
-    score1_rectangle = score1_surface.get_rect(topleft=(150,0))
+    score1_surface = crimson_medium.render('Enemies Killed: '+str(enemies_killed),True,white)
+    score1_rectangle = score1_surface.get_rect(topleft=(200,0))
+    room_number_surf = crimson_medium.render(f'Room {current_room_number}', True, light_blue)
+    room_number_rect = room_number_surf.get_rect(center=(500, 150))
+    screen.blit(room_number_surf, room_number_rect)
     screen.blit(score1_surface,score1_rectangle)
 
 
@@ -116,12 +120,15 @@ def check_if_collision_bullet(room_list,room_number,red_bullets):
                 for bullet in red_bullets:
                     if obstacle.colliderect(bullet[0]):
                          red_bullets.remove(bullet)
+
+
 def check_if_collision_bullet_enemy(enemy_rect,red_bullets,spawn_timer):
     for bullet in red_bullets:
         if enemy_rect.colliderect(bullet[0]):
             red_bullets.remove(bullet)
             spawn_timer=True
             return True
+
 
 def rot_center(image, angle, x, y):
 
@@ -143,14 +150,20 @@ def handle_bullets(red_bullets):
 # Enemy
 
 
-def move_towards_player(enemy, player):
+def move_towards_player(enemy, player, diff):
     dx, dy = player.x - enemy.x, player.y - enemy.y
     dist = math.hypot(dx, dy)
-    if enemy.colliderect(player):
+    if dist <= 40:
         return True
     dx, dy = dx / dist, dy / dist
-    enemy.x += dx * 4
-    enemy.y += dy * 4
+    if diff == 'Easy':
+        fly_speed = 3
+    elif diff == 'Normal':
+        fly_speed = 3.5
+    elif diff == 'Hard':
+        fly_speed = 4
+    enemy.x += dx * fly_speed
+    enemy.y += dy * fly_speed
 
 
 # Colors
@@ -180,7 +193,7 @@ play_button_rect = play_button_surf.get_rect(center=(500, 200))
 settings_button_surf = crimson_medium.render('Settings', True, white)
 settings_button_rect = settings_button_surf.get_rect(center=(500, 250))
 back_button_surf = crimson_medium.render('Back', True, white)
-back_button_rect = back_button_surf.get_rect(bottomleft=(0, 60))
+back_button_rect = back_button_surf.get_rect(bottomleft=(50, 60))
 shop_button_surf = crimson_medium.render('Shop', True, white)
 shop_button_rect = shop_button_surf.get_rect(center=(500, 300))
 tutorial_button_surf = crimson_medium.render('Tutorial', True, white)
@@ -199,9 +212,9 @@ tutorial_txt4_rect = tutorial_txt1.get_rect(center=(500,350))
 tutorial_txt5 = crimson_medium.render('Mind your health. Strange spirits lurk here...',True,white)
 tutorial_txt5_rect = tutorial_txt1.get_rect(center=(500,400))
 
-player_surf = pygame.image.load('graphics/player/spirit_nobg.png')
+player_surf = pygame.image.load('graphics/player/player.png')
 player_rect = player_surf.get_rect(bottomleft=(0, 550))
-enemy_surf = pygame.image.load('graphics/enemies/plasma_blast.jpg')
+enemy_surf = pygame.image.load('graphics/enemies/melee_enemy.png')
 enemy_center = (randint(0, 1000), 200)
 enemy_rect = enemy_surf.get_rect(center=enemy_center)
 
@@ -218,10 +231,9 @@ red_bullets = []
 pygame.display.set_icon(enemy_surf)
 now_move = False
 spawn_timer = True
-room_number1 = 0
-enemies_killed=0
+enemies_killed = 0
+new_image=pygame.transform.flip(player_surf,True,False)
 while True:
-    print(spawn_timer)
     # if isjump == False:
     #     if keys_pressed[pygame.K_SPACE] and player_rect.bottom>=550:
     #         isjump=True
@@ -244,10 +256,10 @@ while True:
     rel_x, rel_y=mouse_x-player_rect.x,mouse_y-player_rect.y
     angle=(180/math.pi)*-math.atan2(rel_y,rel_x)
     angle1=(math.atan2(rel_y,rel_x))
-    print(player_rect.bottom)
     z,n=rot_center(gun_surf,angle,player_rect.x+player_rect.width/2,player_rect.y+player_rect.height/2)
+    
     if spawn_timer==True and type(screen_type) == int:
-        enemy_surf = pygame.image.load('graphics/enemies/plasma_blast.jpg')
+        enemy_surf = pygame.image.load('graphics/enemies/melee_enemy.png')
         enemy_center = (randint(0, 1000), -100)
         enemy_rect = enemy_surf.get_rect(center=enemy_center)
         spawn_timer=False
@@ -259,12 +271,16 @@ while True:
         if event.type == pygame.KEYDOWN:
             if keys_pressed[pygame.K_SPACE]:
                 now_move = True
-                if player_rect.bottom == 550 or player_rect.bottom == 501 or player_rect.bottom == 451 or player_rect.bottom == 401:
+                if player_rect.bottom == 550 or player_rect.bottom == 501 or player_rect.bottom == 451 \
+                        or player_rect.bottom == 401:
                     player_gravity = -20
             if keys_pressed[pygame.K_LEFT]:
                 player_speed = -4 - add_speed
+                new_image=player_surf
+                
             if keys_pressed[pygame.K_RIGHT]:
                 player_speed = 4 + add_speed
+                new_image=pygame.transform.flip(player_surf,True,False)
             if keys_pressed[pygame.K_BACKSLASH]:
                 print('Developer tools enabled.')
                 dev_tools = True
@@ -303,14 +319,14 @@ while True:
                     pygame.quit()
                     exit()
             elif screen_type == 'settings':
-                if fps_rect.collidepoint(pygame.mouse.get_pos()):
-                    if fps_presses % 3 == 0:
-                        fps = 144
-                    elif fps_presses % 3 == 1:
-                        fps = 30
+                if diff_rect.collidepoint(pygame.mouse.get_pos()):
+                    if diff_presses % 3 == 0:
+                        difficulty = 'Hard'
+                    elif diff_presses % 3 == 1:
+                        difficulty = 'Easy'
                     else:
-                        fps = 60
-                    fps_presses += 1
+                        difficulty = 'Normal'
+                    diff_presses += 1
                 elif vol_rect.collidepoint(pygame.mouse.get_pos()):
                     volume = vol_presses % 6
                     vol_presses += 1
@@ -360,15 +376,17 @@ while True:
             player_gravity += 1
             player_rect.y += player_gravity
             player_rect.x += player_speed
-            screen.blit(player_surf, player_rect)
             screen.blit(z, n)
+            screen.blit(new_image, player_rect)
+
             if player_rect.bottom >= 550:
                 player_rect.bottom = 550
                 player_gravity = 0
         else:
             player_gravity = -1
-            screen.blit(player_surf, player_rect)
             screen.blit(z, n)
+            screen.blit(new_image, player_rect)
+
 
         # Room
         if player_rect.x > 1050:
@@ -384,7 +402,6 @@ while True:
             screen_type = checkpoint_room
             dead = False
             current_room_number = checkpoint_room
-            room_number1=checkpoint_room
         screen.blit(game_bg, game_bg_rect)
         screen.blit(ground_bg, ground_bg_rect)
 
@@ -397,22 +414,24 @@ while True:
         # Physics Engine
         player_next_step = pygame.Rect(player_rect.x + player_speed, player_rect.y + player_gravity, player_rect.width,
                                        player_rect.height)
-        collision = check_if_collision(room_list, player_next_step, room_number1)
+        collision = check_if_collision(room_list, player_next_step, current_room_number)
 
         # Collision Detection
         if not collision:
             player_gravity += 1
             player_rect.y += player_gravity
             player_rect.x += player_speed
-            screen.blit(player_surf, player_rect)
             screen.blit(z, n)
+            screen.blit(new_image, player_rect)
+
             if player_rect.bottom >= 550:
                 player_rect.bottom = 550
                 player_gravity = 0
         else:
             player_gravity = -1
-            screen.blit(player_surf, player_rect)
             screen.blit(z, n)
+            screen.blit(new_image, player_rect)
+
         # Exit Detection
         if player_rect.x > 1050:
             if len(room_list) == screen_type + 1:
@@ -423,10 +442,8 @@ while True:
                     room_list.append(starting_structure)
             screen_type += 1
             player_rect.x = 0
-            room_number1 += 1
             current_room_number+=1
         elif player_rect.x < -50:
-            room_number1 -=1
             if screen_type == 0:
                 player_rect.x = -25
             else:
@@ -438,7 +455,7 @@ while True:
         #Enemy Collision & Damage
 
         invincibility -= 1
-        if move_towards_player(enemy_rect, player_rect) and invincibility <= 0:
+        if move_towards_player(enemy_rect, player_rect, difficulty) and invincibility <= 0:
             hp -= 10
             spawn_timer=True
         if check_if_collision_bullet_enemy(enemy_rect,red_bullets,spawn_timer):
@@ -465,9 +482,9 @@ while True:
         screen.blit(back_button_surf, back_button_rect)
         vol_surf = crimson_medium.render(f'Volume: {volume * 20}', True, white)
         vol_rect = vol_surf.get_rect(center=(500, 300))
-        fps_surf = crimson_medium.render(f'FPS: {fps}', True, white)
-        fps_rect = fps_surf.get_rect(center=(500, 250))
-        screen.blit(fps_surf, fps_rect)
+        diff_surf = crimson_medium.render(f'Difficulty: {difficulty}', True, white)
+        diff_rect = diff_surf.get_rect(center=(500, 250))
+        screen.blit(diff_surf, diff_rect)
         screen.blit(vol_surf, vol_rect)
     elif screen_type == 'shop':
         if main_menu_rect.x > -111 and back_counter % 111 == 0:
@@ -561,8 +578,9 @@ while True:
             screen.blit(lv_5_surf, lv_5_rect)
     for bullet in red_bullets:
         pygame.draw.rect(screen,(255,255,255),bullet[0])
-    check_if_collision_bullet(room_list,room_number1,red_bullets)
+    check_if_collision_bullet(room_list,current_room_number,red_bullets)
 
     handle_bullets(red_bullets)
     pygame.display.update()
-    clock.tick(fps)
+    clock.tick(60)
+
